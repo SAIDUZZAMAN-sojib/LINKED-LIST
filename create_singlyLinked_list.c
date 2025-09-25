6dd4fdb2a65b97e6ddf7d8f3c82fb2d4b85476c0
MD SAIDUZZMAN <saiduzzaman.015.sojib@gmail.com>
2025-09-25 10:37:52 +0600

    Update create_singlyLinked_list.c

--- create_singlyLinked_list.c
+++ create_singlyLinked_list.c
@@ -1,65 +1,70 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-struct node {
-    int data;
+#include<stdlib.h>
+struct node 
+{
+    int data ;
     struct node *next;
 };
+struct node *head;
 
-struct node *header = NULL;
-
-void createList(int n) {
-    struct node *temp, *newNode;
-    int data, i;
-
-    if (n <= 0) {
-        return;
+void createlist(int n)
+{
+    struct node *newnode,*temp;
+    int i,data;
+    newnode=(struct node *)malloc(sizeof(struct node ));
+    if (newnode==NULL)
+    {
+       return ;
     }
+    else
+    {
+        printf("Enter the the data of node 1 :");
+        scanf("%d",&data);
+        newnode->data=data;
+        newnode->next=NULL;
+        head=newnode;
+        temp=newnode;
+        for (int  i = 2; i <=n; i++)
+        {
+            newnode=(struct node *)malloc(sizeof(struct node ));
+            if (newnode==NULL)
+            {
+               return;
 
-    header = (struct node *)malloc(sizeof(struct node));
-    if (header == NULL) {
-        return;
-    }
+            }
+            else 
+            {
+                printf("Enter the data of node %d ",i);
+                scanf("%d",&data);
+                newnode->data=data;
+                newnode->next=NULL;
+                temp->next=newnode;
+                temp=newnode;
 
-    printf("Enter the data of node 1: ");
-    scanf("%d", &data);
-    header->data = data;
-    header->next = NULL;
-    temp = header;
 
-    for (i = 2; i <= n; i++) {
-        newNode = (struct node *)malloc(sizeof(struct node));
-        if (newNode == NULL) {
-            return;
+            }
+            
         }
-        printf("Enter the data of node %d: ", i);
-        scanf("%d", &data);
-        newNode->data = data;
-        newNode->next = NULL;
-        temp->next = newNode;
-        temp = newNode;
+        
     }
+    
 }
-
-void displayList() {
-    struct node *temp = header;
-    if (temp == NULL) {
-        printf("List is empty.\n");
-        return;
-    }
-    printf("\nLinked list contents: ");
-    while (temp != NULL) {
-        printf("%d -> ", temp->data);
-        temp = temp->next;
+void display()
+{
+    struct node *temp=head;
+    while (temp!=NULL)
+    {
+        printf("%d->",temp->data);
+        temp=temp->next;
     }
     printf("NULL\n");
+    
 }
-
-int main() {
+int main() { 
     int n;
-    printf("Enter the total number of nodes: ");
-    scanf("%d", &n);
-    createList(n);
-    displayList();
+    printf("Enter node size:");
+    scanf("%d",&n);
+    createlist(n);
+    display();
     return 0;
 }
